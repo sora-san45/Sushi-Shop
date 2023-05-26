b2f1b9040918d5c2e42a60042450a6085708a684
Mary Ann Jose <maryannjose129@gmail.com>
2023-05-26 15:23:02 +0530

    commits

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,32 +12,77 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List sushiList = [
-    ["sushi1.png", "\$ 2.6"],
-    ["sushi2.png", "\$ 3.1"],
-    ["sushi3.png", "\$ 2.8"],
-    ["sushi4.png", "\$ 3.0"]
+    ["sushi1.png","Sushi1","\$ 2.6","4.8"],
+    ["sushi2.png","Sushi2","\$ 3.1","4.0"],
+    ["sushi3.png","Sushi3","\$ 2.8","4.1"],
+    ["sushi4.png","Sushi4","\$ 3.0","4.2"]
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.only(left: 14),
-          child: Column(
+        body:Column(
             children: [
               SizedBox(height: 300),
-              Text("Top picks"),
-              GridView.builder(
-                itemCount: sushiList.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+              Padding(
+                padding: const EdgeInsets.only(left:38.0),
+                child: Container(
+                  width:350,
+                  height:400,
+                  child: GridView.builder(
+                      itemCount: 4,
+                      scrollDirection: Axis.vertical,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 10,
+                      ),
+                      itemBuilder: (context, index) {
+                        return Stack(
+                          fit:StackFit.loose,
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                            height:130,
+                            width:130,
+                            decoration: BoxDecoration(
+                              color:Color.fromARGB(255, 239, 232, 232),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          Positioned(
+                              top:-50,
+                            left:7,
+                            child: Container(
+                              height:115,
+                              width:115,
+                              decoration: BoxDecoration(
+                                image:DecorationImage(image:AssetImage("lib/images/${sushiList[index][0]}"))
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top:72,
+                            left:12,
+                            child: Column(children: [
+                            Text("${sushiList[index][1]}",style: TextStyle(fontSize: 14.5),),
+                            SizedBox(height:6,),
+                            Text("${sushiList[index][2]}",style: TextStyle(fontSize:17,fontWeight: FontWeight.w500),)
+                          ],)),
+                          Positioned(
+                            left:75,
+                            top:92,
+                            child:Row(children: [
+                            Icon(Icons.star,color: Colors.yellow,),
+                            Text("${sushiList[index][3]}")
+                          ],))
+                          ]
+                        );
+                      }),
                 ),
-                itemBuilder: (context, index) {
-                  SushiTile(sushiList[index]);
-                },
-              )
+              ),
             ],
           ),
-        ));
+      );
   }
 }
--- lib/sushitile.dart
+++ lib/sushitile.dart
@@ -3,11 +3,19 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class SushiTile extends StatelessWidget {
-  const SushiTile(sushiList, {super.key});
+  const SushiTile(  {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
+      Container(
+        height:150,
+        width: 150,
+        decoration: BoxDecoration(
+          color:Color.fromARGB(202, 188, 117, 117),
+          
+        ),
+      )
     ]);
   }
 }

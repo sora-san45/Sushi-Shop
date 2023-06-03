3fdaf886aef3b710dd22689ef0ec025a0aafe76e
Mary Ann Jose <maryannjose129@gmail.com>
2023-06-03 08:53:40 +0530

    commit

---
+++ fast_server/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class Sushi(BaseModel):
+    sushiImage: str
+    sushiName : str
+    sushiType: str
+    sushiPrice : float
+    sushiRating : float
+
+sushiList=[]
+maki=[]
+nigiri=[]
+gunkan=[]
+temaki=[]
+
+@app.get("/get_all")
+def root():
+    return "hello"
